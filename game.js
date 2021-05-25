255ed190e5ef6d9a8153748c9691ecb8e7b952fa
tahiru94 <tahiru1994@engineer.com>
2021-05-25 17:32:28 -0400

    Add update function for entities

--- game.js
+++ game.js
@@ -6,6 +6,7 @@
         this.y = y;
         this.width = 20;
         this.height = 20;
+        this.direction = -1;
     }
 
     Player.prototype.draw = function (canvasContext) {
@@ -13,17 +14,32 @@
         canvasContext.fillRect(this.x, this.y, this.width, this.height);
     }
 
+    Player.prototype.update = function (canvas) {
+        this.y = this.y + this.direction;
+        if (this.y <= 0 || (this.y + this.height >= canvas.height)) {
+            this.direction *= -1;
+        }
+    }
+
     function Enemy(x, y) {
         this.x = x;
         this.y = y;
         this.width = 10;
         this.height = 10;
+        this.direction = 1;
     }
 
     Enemy.prototype.draw = function (canvasContext) {
         canvasContext.fillStyle = "red";
         canvasContext.fillRect(this.x, this.y, this.width, this.height);
     };
+
+    Enemy.prototype.update = function (canvas) {
+        this.y = this.y + this.direction;
+        if (this.y <= 0 || (this.y + this.height >= canvas.height)) {
+            this.direction *= -1;
+        }
+    }
 
     const canvas = document.querySelector('#game-layer');
     const context = canvas.getContext('2d');
@@ -34,13 +50,9 @@
 
     // Add the player entity
     const player = new Player(100, 175);
-    player.draw(context);
 
     // Add enemy entities
     const enemy1 = new Enemy(20, 25);
     const enemy2 = new Enemy(80, 25);
     const enemy3 = new Enemy(160, 25);
-    enemy1.draw(context);
-    enemy2.draw(context);
-    enemy3.draw(context);
 }());
